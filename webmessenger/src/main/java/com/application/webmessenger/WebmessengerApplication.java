1a3cfdac72fb40a317aaab7f956d559463eeef31
Ion Vasluian <79792299+VasluianI0n@users.noreply.github.com>
2023-11-24 15:24:09 +0200

    added frontend

---
+++ webmessenger/src/main/java/com/application/webmessenger/WebmessengerApplication.java
@@ -0,0 +1,13 @@
+package com.application.webmessenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebmessengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebmessengerApplication.class, args);
+	}
+
+}
---
+++ webmessenger/src/test/java/com/application/webmessenger/WebmessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.webmessenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebmessengerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
